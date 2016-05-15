20ea900b8ef8ab2e0b085108e1b6cd23448c83c8
hcopperm <hannah@shuddle.us>
2016-05-14 21:39:56 -0700

    Button color

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,10 @@
 
 $(document).on('ready page:load', function () {
   $(".datepicker").datepicker();
+
+  $(".scope").on("click", function() {
+    $(".scope").removeClass("selected");
+    $(this).addClass("selected");
+  });
+
 });
